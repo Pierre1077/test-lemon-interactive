95c79928015ebb5129820b2d3e3ddc40becd298b
psenechal10 <p.senechal@namkin.fr>
2023-06-18 22:08:37 +0200

    feat: add favicon and title

--- src/components/Recommendation/Recommendation.jsx
+++ src/components/Recommendation/Recommendation.jsx
@@ -1,13 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import MovieCard from "../MovieCard/MovieCard";
 import {useNavigate} from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import SwiperCore, {  Navigation } from 'swiper';
+import 'swiper/swiper.min.css';
+import 'swiper/swiper-bundle.min.css';
+import Swiper from 'swiper';
+
+SwiperCore.use([Navigation]);
 
 const Recommendation = () => {
     const [favoriteMovies, setFavoriteMovies] = useState([]);
     const [recommendationMovies, setRecommendationMoviesData] = useState([]);
     const navigate = useNavigate();
+    const swiperRef = useRef(null);
 
     const favoriteCategories = Array.from(
         new Set(favoriteMovies.map((favMovie) => favMovie.genres).flat())
@@ -63,26 +70,62 @@ const Recommendation = () => {
         <FontAwesomeIcon icon={faThumbsUp} className="removeToFavorite" />
     );
 
+    useEffect(() => {
+        const swiper = new Swiper(swiperRef.current, {
+            slidesPerView: 4,
+            spaceBetween: 16,
+            navigation: {
+                nextEl: ".swiper-button-next",
+                prevEl: ".swiper-button-prev",
+            },
+            breakpoints: {
+                540: {
+                    slidesPerView: 1,
+                },
+                768: {
+                    slidesPerView: 2,
+                },
+                1024: {
+                    slidesPerView: 3,
+                },
+                1280: {
+                    slidesPerView: 4,
+                }
+            }
+        });
+
+        return () => {
+            swiper.destroy();
+        };
+    }, []);
+
     return (
-        <div>
-
-            <h2>Recommendation Movies</h2>
-            <div style={{display: "flex"}}>
-                {recommendationMovies.map((movie) => (
-                    <MovieCard
-                        key={movie.id}
-                        title={movie.name}
-                        genres={movie.genres ? movie.genres.join(', ') : ''}
-                        rating={movie.rating ? movie.rating.average : ''}
-                        navigateToDetail={() => navigateToDetail(movie)}
-                        thumbnail={movie.image ? movie.image.original : 'https://via.placeholder.com/100'}
-                        addToFavorite={() => addToFavorites(movie)}
-                        isFavorite={favoriteMovies.some((favMovie) => favMovie.id === movie.id) ? addToFavoriteIcon : removeToFavoriteIcon}
-                    />
-                ))}
+        <div style={{width: "90%", margin: '0 auto'}}>
+            <div className="movieTitle">
+                <h2>Recommendation Movies</h2>
             </div>
+            <div className="swiper mySwiper" ref={swiperRef} >
+                <div className="swiper-wrapper">
+                    {recommendationMovies.map((movie) => (
+                        <div className="swiper-slide" key={movie.id}>
+                            <MovieCard
+                                title={movie.name}
+                                genres={movie.genres.join(', ')}
+                                rating={movie.rating.average}
+                                thumbnail={movie.image.original}
+                                navigateToDetail={() => navigateToDetail(movie)}
+                                addToFavorite={() => addToFavorites(movie)}
+                                isFavorite={favoriteMovies.some((favMovie) => favMovie.id === movie.id) ? addToFavoriteIcon : removeToFavoriteIcon}
+                            />
+                        </div>
+                    ))}
+                </div>
 
+                <div className="swiper-button-next"></div>
+                <div className="swiper-button-prev"></div>
+            </div>
         </div>
+
     );
 };
 
